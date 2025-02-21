422f7cb2fafba0a3b0c0fc78126a625accbbd2d1
Josenunez-25 <jose.nunez@ciencias.unam.mx>
2025-02-20 20:31:24 -0600

    Update Coleccion.java

--- Coleccion.java
+++ Coleccion.java
@@ -3,7 +3,17 @@
  *
  * Interfaz que define los métodos que debe implementar una colección
  * genérica.
+ + @authors José Manuel Núñez Ruíz, Diego Alexander Cisneros Tenorio
  */
 public interface Coleccion<T> extends Iterable<T> {
-
+    //metodo insertar
+    public void insertar(T elemento);
+    //metodo eliminar 
+    public void eliminar (int indice);
+    //metodo acceder
+    public T acceder (int indice);
+    //metodo buscar
+    public boolean buscar (T elemento);
+    //metodo vaciar
+    public void vaciar();
 }
